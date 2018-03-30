70ecbbf937a418f25f29af6374cf6b3f85714d7f
TSchaeken-Deskop <TSchaeken@gmail.com>
2018-03-30 00:45:08 -0700

    Added score functionality and correct answers ID

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,36 @@
 var gameData = {
-    timeUp(){
-        $("#game-container").text("Your time is up!")
+    score: 0,
+    timeUp() {
         clearInterval(IntervalId);
+        $("#time-remaining").text("Your time is up!");
+    },
+    tallyScore() {
+        for (var i = 1; i < 11; i++) {
+            var RadioOptions = "RadioOptions" + [i];
+            if ($('input[name=inline' + RadioOptions + ']:checked').attr('id') == "CorrectAnswer") {
+                this.score++
+            }
+        }
     }
 }
 
 var IntervalId;
 var counter = 5;
 
-function decrement(){
-    $('#question-row').text(counter)
-    if (counter == 0) {
+function decrement() {
+    $('#time-remaining').text("Time remaining: " + counter)
+    if (counter < 0) {
         gameData.timeUp()
     }
     counter--
 }
 
-$(".jumbotron").on("click", "#start-btn", function(){
+$(".jumbotron").on("click", "#start-btn", function () {
+    $("#trivia-form").css({ display: "block" })
     intervalId = setInterval(decrement, 1000);
+})
+
+$("#trivia-form").on("click", "#submitBtn", function () {
+    gameData.tallyScore()
+    $(this).prop('disabled', true);
 })
